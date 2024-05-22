ad46819b1b1b6fdbc345ddd197dc1b09b0c7b402
Simon Emms <simon@simonemms.com>
2024-05-22 09:52:30 +0000

    feat(js): bump svelte dependencies

--- js/{{ cookiecutter.project_name }}/svelte/svelte.config.js
+++ js/{{ cookiecutter.project_name }}/svelte/svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/vite-plugin-svelte').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
